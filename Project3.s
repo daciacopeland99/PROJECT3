c939c9dc3b8d7ff0a02ea159438c27251b684250
daciacopeland99 <44177869+daciacopeland99@users.noreply.github.com>
2018-12-14 22:58:37 -0500

    Error
    As I tried something new in my previous code, after running it does not work. Code from my original work earlier this week, which does work.

--- Project3.s
+++ Project3.s
@@ -1,4 +1,3 @@
-#this program takes user's input, compares it to conditions, and prints out appropriate message/output
 .data
     emptyInput: .asciiz "Input is empty." #message for string with an empty input
     longInput: .asciiz "Input it is too long." #messgae for string that has more than 4 characters
@@ -13,7 +12,7 @@
         add $t0, $0, 0 #initialize $t0 register
         add $t1, $0, 0 #initialize $t1 register
 
-        la $t2, userInput #load string into $t2 register
+        la $t2, userInput #load string addr into $t2 register
         lb $t0, 0($t2)
         beq $t0, 10, isEmpty #check for an empty string
         beq $t0, 0 isEmpty
@@ -45,7 +44,7 @@
             addi $t1, $t1, 1
             beq $t0, 10, restart
             beq $t0, 0, restart
-            bne $t0, 32, CorrectMessage #jump to CorrectMessage branch to print out either isInvalid or IsTooLong
+            bne $t0, 32, isInvalid #jump to isInvalid branch if not equal
             j viewRemaining
 
         restart:
@@ -61,45 +60,31 @@
         stringLength:
             lb $t0, ($t2)
             addi $t2, $t2, 1
-            addi $t1, $t1, 1
+            addi $t1, $t1, 1 
             beq $t0, 10, callconversionfunc
             beq $t0, 0, callconversionfunc
             beq $t0, 32, callconversionfunc
             beq $t1, 5, isTooLong
-            move $a1, $t1 #store the length in the $a1 register to be passed in subprogram
+            move $a1, $t1
             j stringLength
-            
-            CorrectMessage: #remove doLoop and replace with CorrectMessage branch
-            beq $t1, 5, isTooLong #check to see if the length is greater than 4
-            beq $t1, 1, isInvalid
-            beq $t1, 2, isInvalid
-            beq $t1, 3, isInvalid
-            beq $t1, 4, isInvalid
-            
-      callconversionfunc:
-        sub $t2, $t2, $t1 #move ptr back to start of string
-        addi $sp, $sp, -4 #allocating memory for stack
-        sw $ra, 0($sp) #return address
-        
-        move $a0, $t2
-        li $a2, 1 #exponentiated base
 
-        addi $sp, $sp, -12
-        sw $a1, 0($sp) #string length
-        sw $a2, 4($sp) #ex. base
-        sw $a0, 8($sp)
-        jal DecimalVersion #call to function
-        lw $v0, 0($s0)
-        addi $sp, $sp, 4
-        
-        #print result
-        move $a0, $v0
-        li $v0, 1
-        syscall
-        
-        lw $ra, 0($sp)
-        addi $sp $sp 4 #deallocating the memory
-        jr $ra
+callconversionfunc:
+    sub $t2, $t2, $t1 #move ptr back to start of string
+    addi $sp, $sp, -4 #allocating memory for stack
+    sw $ra, 0($sp) #only return address
+    move $a0, $t2
+    #li $a1, 3 #string length !! 
+    li $a2, 1 #exponentiated base
+    jal DecimalVersion #call to function
+
+    #print result
+    move $a0, $v0
+    li $v0, 1
+    syscall
+    
+    lw $ra, 0($sp) 
+    addi $sp, $sp, 4 #deallocating the memory
+    jr $ra
 
         end:
             move $a0, $t5 #move value to $a0
@@ -107,11 +92,12 @@
             syscall
             li $v0, 10 #end program
             syscall
-            
+
         isEmpty:
             la $a0, emptyInput #loads string/message
             li $v0, 4 #prints string
             syscall
+
             li $v0, 10 #end of program
             syscall
 
@@ -132,13 +118,8 @@
             syscall
 
             jr $ra
-            
-  DecimalVersion:
-    lw $a1, 0($sp)
-    lw $a2, 4($sp)
-    lw $a0, 8($sp)
-    addi $sp, $sp, 12
-    
+
+DecimalVersion:
     addi $sp, $sp, -8 #allocating memory for stack
     sw $ra, 0($sp) #storing return address
     sw $s3, 4($sp) #storing s register so it is not overwritten
@@ -147,53 +128,43 @@
     add $t0, $a0, $a1 #getting address of the last byte 
     lb $s3, 0($t0)  #loading the byte ^
     #INSERT THE CODE TO CONVERT BYTE TO DIGIT
-            blt $t0, 48, isInvalid #if char is before 0 in ascii table, the input is invalid
-            blt $t0, 58, number
-            blt $t0, 65, isInvalid
-            blt $t0, 90, upperCase
-            blt $t0, 97, isInvalid
-            blt $t0, 122, lowerCase
-            blt $t0, 128, isInvalid
+    #asciiConversions:
+            blt $s3, 48, isInvalid #if char is before 0 in ascii table, the input is invalid
+            blt $s3, 58, number
+            blt $s3, 65, isInvalid
+            blt $s3, 90, upperCase
+            blt $s3, 97, isInvalid
+            blt $s3, 122, lowerCase
+            blt $s3, 128, isInvalid
 
         upperCase:
-            addi $t0, $t0, -55
-            j More #jump to more branch
+            addi $s3, $s3, -55
+            jal More
 
         lowerCase:
-            addi $t0, $t0, -87
-            j More
+            addi $s3, $s3, -87
+            jal More
 
         number:
-            addi $t0, $t0, -48
-            j More
-        More:
-            mul $s3, $s3, $a2 #multiplying the byte x the exponentiated base (starts at 1(35^0 = 1))
-            mul $a2, $a2, 35 #multiplying the exponentiated base by 35 to get next power (35^1 ...)
-            addi $sp, $sp, -12
-            sw $a1, 0($sp)
-            sw $a2, 4($sp)
-            sw $a0, 8($sp)
-            jal DecimalVersion
-            lw $v0, 0($s0)
-            addi $sp, $sp, 4
-            
-            add $v0, $s3, $v0   #returning last byte plus decimal version of the rest of number
-            lw $ra, 0($sp)      
-            lw $s3, 4($sp)
-            addi $sp, $sp, 8
-
-            addi $sp, $sp, -4
-            
-            sw $v0, 0($sp)
-            jr $ra
-
-     return_zero:
-       li $v0, 0
-       lw $ra, 0($sp)
-       lw $s3, 4($sp)
-       addi $sp, $sp, 8
-
-       addi $sp, $sp, -4
-        sw $v0, 0($sp)
+            addi $s3, $s3, -48
+            jal More
+    #mul $s3, $s3, $a2 #multiplying the byte x the exponentiated base (starts at 1(35^0 = 1))
+    #mul $a2, $a2, 35 #multiplying the exoonentiated base by 35 to get next power (35^1 ...)
+    More:
+        mul $s3, $s3, $a2 #multiplying the byte x the exponentiated base (starts at 1(35^0 = 1))
+        mul $a2, $a2, 35 #multiplying the exponentiated base by 35 to get next power (35^1 ...)
+        jal DecimalVersion
+    # a0=str addr, a1=strlen, a2=exponentiated base
+    
+    #jal DecimalVersion #call function again (loop)
+        add $v0, $s3, $v0   #returning last byte plus decimal version of the rest of number
+        lw $ra, 0($sp)      
+        lw $s3, 4($sp)
+        addi $sp, $sp, 8
         jr $ra
-           
+return_zero:
+    li $v0, 0
+    lw $ra, 0($sp)
+    lw $s3, 4($sp)
+    addi $sp, $sp, 8
+    jr $ra
